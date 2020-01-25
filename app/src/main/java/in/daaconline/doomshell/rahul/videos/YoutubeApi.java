c79cd8f4dc38f58873a13e3bf5000414295f6329
rahulpansari <32799743+rahulpansari@users.noreply.github.com>
2020-01-25 22:47:31 +0530

    Update YoutubeApi.java
    YOUTUBE API FROM DEVELOPER CONSOLE

--- app/src/main/java/in/daaconline/doomshell/rahul/videos/YoutubeApi.java
+++ app/src/main/java/in/daaconline/doomshell/rahul/videos/YoutubeApi.java
@@ -2,7 +2,7 @@ package in.daaconline.doomshell.rahul.videos;
 
 public class YoutubeApi {
 
-    public static  final String YOUTUBE_API="AIzaSyCz6eNHNW5qstY0XlU8gMaS056q42rVLek";
+    public static  final String YOUTUBE_API="YOUR-API-FROM GOOGLE DEVELOPER CONSOLE";
 
     public static final  String YOUTUBE_URL="https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=PLfuE3hOAeWhYCPPLA75AXfd0pILeyePjv&key=AIzaSyAvnmGEUO5Sc7hxJVMvECRTG5LG0aYEUuk";
 }
